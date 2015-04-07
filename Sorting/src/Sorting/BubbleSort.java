7a15ba6b7e3c7ceed86fbca358abfeac43a67eb1
Nickie Pucel <nicholas_pucel@brown.edu>
2015-04-07 13:28:12 -0400

    implemented bubblesort

--- Sorting/src/Sorting/BubbleSort.java
+++ Sorting/src/Sorting/BubbleSort.java
@@ -8,7 +8,21 @@ public class BubbleSort {
 	 */
 	public static int[] bubbleSort(int[] unsortedArray) {
 		//TODO: S1 implements bubble sort
-		
-		return null;
+		if (unsortedArray == null) {
+			return null;
+		}
+		boolean swapped = true;
+		while (swapped) {
+			swapped = false;
+			for (int i = 1; i < unsortedArray.length; i++) {
+				if (unsortedArray[i] < unsortedArray[i-1]) {
+					int temp = unsortedArray[i];
+					unsortedArray[i] = unsortedArray[i-1];
+					unsortedArray[i-1] = temp;
+					swapped = true;
+				}
+			}		
+		}
+		return unsortedArray;
 	}
 }
